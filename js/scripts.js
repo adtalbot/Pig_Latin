f2d5ecaeea508ff136a29272e6ded2835c84a7e7
audrie <audrie.talbot@gmail.com>
2015-07-01 15:47:10 -0700

    Passes specs for pigLatin and hasConsonantAt functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 var pigLatin = function(englishWord) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  if(hasConsonantAt(englishWord, 1)) {
+    var translatedWord = englishWord.slice(2) + englishWord.slice(0, 2);
+  } else if (hasConsonantAt(englishWord, 0)) {
+    var translatedWord = englishWord.slice(1) + englishWord.slice(0, 1);
+  } else if (!hasConsonantAt(englishWord, 0)) {
+    var translatedWord = englishWord;
+  }
+    return translatedWord + 'ay';
+};
 
-  for(var i = 0; i < vowels.length; i++) {                      /*Intializes index to 0, loops while i is less than 5, increments i by 1 after each loop*/
-    if(englishWord[0] === vowels[i]) {                          /*Evaluates if first letter of englishWord is equal to vowel array at position = i*/
-      var startsWithVowel = true;                               /*startsWithVowel will be true or false*/
-      break;
+var hasConsonantAt = function(word, position) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];                       /*Intializes index to 0, loops while i is less than 5, increments i by 1 after each loop*/
+  for (var i = 0; i < vowels.length; i++) {                     /*Evaluates if first letter of englishWord is equal to vowel array at position = i*/
+    if(word[position] === vowels[i]) {                          /*startsWithVowel will be true or false*/
+      return false;
     }
   }
-
-  if(startsWithVowel) {                                         /*If startsWithVowel evaluates to true*/
-    var translatedWord = englishWord;                           /*Creates variable translatedWord to englishWord*/
-  } else {                                                      /*Else evaluates statement below*/
-    var translatedWord = englishWord.slice(1) + englishWord[0]; /*Takes first letter off of englishWord, concates letter at position 0 of englishWord. Stores in translatedWord*/
-  }
-  return translatedWord + 'ay';                                 /*After if statement terminates, returns translatedWord with 'ay' concated to end*/
+  return true;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -5,9 +5,23 @@ describe('pigLatin', function() {
 
   it('adds ay to the end of a word that starts with any vowel', function() {
     pigLatin('english').should.equal('englishay');
-  })
+  });
 
   it('takes a single consonant at the beginning of a word and moves it to the end', function() {
     pigLatin('cat').should.equal('atcay');
+  });
+
+  it('takes two consanants at the beginning of a word and moves them to the end', function() {
+    pigLatin('cricket').should.equal('icketcray');
+  });
+});
+
+describe('hasConsonantAt', function() {
+  it('returns true if a word has a consonant in the specified spot', function() {
+    hasConsonantAt('peanut', 0).should.equal(true);
+  });
+
+  it('returns false if a word does not have a consonant in the specified spot', function() {
+    hasConsonantAt('peanut', 1).should.equal(false)
   });
 });
