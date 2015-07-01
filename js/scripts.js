a8006365bbb53a53dbb18795764b827c705333f9
audrie <audrie.talbot@gmail.com>
2015-07-01 11:54:27 -0700

    Passes first two specs.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var pigLatin = function(englishWord) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  for(var i = 0; i < vowels.length; i++) {                      /*Intializes index to 0, loops while i is less than 5, increments i by 1 after each loop*/
+    var startsWithVowel = (englishWord[0] === vowels[i]);       /*Evaluates if first letter of englishWord is equal to vowel array at position = i. startsWithVowel will be true or false*/
+    break;                                                      /*When startsWithVowel evaluates to true, will break for loop*/
+  }
+
+  if(startsWithVowel) {                                         /*If startsWithVowel evaluates to true*/
+    var translatedWord = englishWord;                           /*Creates variable translatedWord to englishWord*/
+  } else {                                                      /*Else evaluates statement below*/
+    var translatedWord = englishWord.slice(1) + englishWord[0]; /*Takes first letter off of englishWord, concates letter at position 0 of englishWord. Stores in translatedWord*/
+  }
+  return translatedWord + 'ay';                                 /*After if statement terminates, returns translatedWord with 'ay' concated to end*/
+};
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pigLatin', function() {
   it('adds ay to the end of a word that starts with a vowel', function() {
     pigLatin('apple').should.equal('appleay');
   });
+
+  it('takes a single consonant at the beginning of a word and moves it to the end', function() {
+    pigLatin('cat').should.equal('atcay');
+  });
 });
